413ab9d219abf4707ce59f26fa619ce99f0ff6c6
Lucas Sorensen <lucas.alex.sorensen@gmail.com>
2021-10-04 11:40:03 +0200

    Add week1

---
+++ w1/e2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    string input;
+    cin >> input;
+
+    cout << "Hello " << input << "!" << endl;
+
+    return 0;
+}
